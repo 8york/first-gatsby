a13b3313784697577c7aa22e3066eb6318ab7e5d
Akiko Yamasaki <paonpaon@gmail.com>
2023-06-02 23:16:07 +1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `first gatsby ye`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
