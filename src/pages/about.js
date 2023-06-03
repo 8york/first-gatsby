31aaa651e7d99b7ec4cd55574a9fadc7df501594
Akiko Yamasaki <paonpaon@gmail.com>
2023-06-03 12:26:25 +1000

    gatsby first push

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage =() =>{
+    return(
+        <Layout pageTitle="About haha">
+            <p>Hello tell me who we are and who you think you are</p>
+        </Layout>
+    )
+}
+
+export const Head =()=><title>who are you and we</title>
+export default AboutPage
